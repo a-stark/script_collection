8ad7af2764f628f12130069a88958821fb0f684d
Alexander Stark <alexander.stark@uni-ulm.de>
2018-02-08 15:42:13 +0100

    Add more docstrings to understand the functionality.

--- file_descriptor/file_descriptor.py
+++ file_descriptor/file_descriptor.py
@@ -80,8 +80,27 @@ def get_raw_fd():
 
 
 def get_available_fd(fd_max=512):
+    """ Retrieve the available file descriptors obtained from the fstat module.
+
+    Parameters
+    ----------
+    fd_max: int, optional
+        the maximal (non-negative) number of file descriptors to check. Specify
+        here, depending on the OS, the correct maximal number. 
+
+    Returns
+    -------   
+    list
+        the entries of the list are tuples(fd, res) containing 
+        fd: int
+            the file descriptor and
+        res: os.stat_result
+            the result object of os.fstat request.
+
+    All the file descriptors from 0 to fd_max are checked and the available 
+    ones are appended to the returned list. 
     """
-    """
+
     fd_list = []
     for i in range(fd_max):
         try:
@@ -92,84 +111,38 @@ def get_available_fd(fd_max=512):
         
     return fd_list
 
-    def get_mapped_fd():
-    """ This should work for all operating systems. """
-
-    raw_fd = get_raw_fd()
-    
-    fd_max = get_fd_max()
-    fd_list = get_available_fd(fd_max)
-        
-    # Try now to match the opened raw files found in raw_fd
-    # with those obtained from fd_list and save the found ones
-    # in the list mapped_fd like
-    #    ()
-    mapped_fd = []
-    for entry in raw_fd:
-        path = entry.path
-        wrong_fd = entry.fd
-        try:
-            f = open(path, 'r')
-            for fd, fstat in fd_list:
-                try:
-                    curr_fd = f.fileno()
-                    if os.path.sameopenfile(curr_fd,fd):
-                        mapped_fd.append((fd, path))
-                except OSError:
-                    break
-            
-            f.close()
-        except FileNotFoundError:
-            continue
-            
-    return mapped_fddef get_mapped_fd():
-    """ This should work for all operating systems. """
-
-    raw_fd = get_raw_fd()
-    
-    fd_max = get_fd_max()
-    fd_list = get_available_fd(fd_max)
-        
-    # Try now to match the opened raw files found in raw_fd
-    # with those obtained from fd_list and save the found ones
-    # in the list mapped_fd like
-    #    ()
-    mapped_fd = []
-    for entry in raw_fd:
-        path = entry.path
-        wrong_fd = entry.fd
-        try:
-            f = open(path, 'r')
-            for fd, fstat in fd_list:
-                try:
-                    curr_fd = f.fileno()
-                    if os.path.sameopenfile(curr_fd,fd):
-                        mapped_fd.append((fd, path))
-                except OSError:
-                    break
-            
-            f.close()
-        except FileNotFoundError:
-            continue
-            
-    return mapped_fd
-
 def get_mapped_fd():
-    """ This should work for all operating systems. """
+    """ Reconstruct the opened file descriptors associated to their file paths.
+
+    Returns
+    -------  
+    list:
+        containing tuples(fd, path) with the meaning
+        fd: int
+            the integer number of the file descriptor 
+        path: str
+            the path to the opened file associated to the file descriptor
+
+    This procedure should work for all operating systems and does not rely on
+    Unix specific or Windows specific implementations for file descriptors.
+    """
 
     raw_fd = get_raw_fd()
-    
     fd_max = get_fd_max()
     fd_list = get_available_fd(fd_max)
         
-    # Try now to match the opened raw files found in raw_fd
-    # with those obtained from fd_list and save the found ones
-    # in the list mapped_fd like
-    #    ()
+    # Try now to match the opened raw files found in raw_fd with those obtained
+    # from fd_list and save the found ones in the list mapped_fd
+
     mapped_fd = []
     for entry in raw_fd:
         path = entry.path
-        wrong_fd = entry.fd
+
+        wrong_fd = entry.fd # this is not used, since it is not always correct
+        
+        # procedure: the trick is in trying to open the available path of a 
+        # file (which will associate an file descriptor with it) and check, 
+        # which file descriptor is the same as the current one.
         try:
             f = open(path, 'r')
             for fd, fstat in fd_list:
@@ -179,17 +152,30 @@ def get_mapped_fd():
                         mapped_fd.append((fd, path))
                 except OSError:
                     break
-            
+    
             f.close()
-        except FileNotFoundError:
+
+        # by running a method in the (i)python interpreter a history of the 
+        # used calls will be created. Eventually, this will be written to a 
+        # history file, which also uses a file descriptor to open and save it. 
+        # This file descriptor can be available at the beginning of the 
+        # execution of this method (obtained in 'get_raw_fd') but it might be 
+        # already closed during the actual checking procedure. Therefore this
+        # exception has to be caught.
+        except FileNotFoundError:   
             continue
             
     return mapped_fd
+
 
 def clean_up_fd():
+    """ Close all file descriptors, which are pointing to opened file paths."""
+
     fd_list = get_mapped_fd()
     for fd, path in fd_list:
         try:
+            print('Closing file file descriptor "fd={0}" from the path:\n{0}'
+                  ''.format(fd, path))
             os.close(fd)
         except OSError:
             print('fd={0} pointing at the path="{1} cannot be closed. '
@@ -198,6 +184,9 @@ def clean_up_fd():
 
 # originally taken from the blog of Vangelis M. (20.05.2014):
 # http://mihalop.blogspot.dk/2014/05/python-subprocess-and-file-descriptors.html
+
+# mapping the methods to a helper variable to check for the correct mode status
+# on the opened file descriptor.
 
 _fd_types = (
     ('BLK', stat.S_ISBLK),      # block special device file
